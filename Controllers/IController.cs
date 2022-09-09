d01f984d03d285730732d8dca63ef2d7abd53fb6
Nicholas Trambitas <ntrambitas12@gmail.com>
2022-09-08 23:50:46 -0400

    added template for this sprint, imported keyboard controller from sprint 0

---
+++ Controllers/IController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+     interface IController
+    {
+   
+    public void Update();
+    }
+
---
+++ Controllers/KeyboardController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+     class KeyboardController: IController, IUpdateable
+    {
+    private Dictionary<Keys, ICommand> controllerMappings;
+
+    public KeyboardController()
+    {
+        controllerMappings = new Dictionary<Keys, ICommand>();
+    }
+
+    public void RegisterCommand(Keys key, ICommand command)
+    {
+            controllerMappings.Add(key, command);
+    }
+
+    public void Update()
+    {
+        Keys[] pressedKeys = Keyboard.GetState().GetPressedKeys();
+
+       
+        foreach (Keys key in pressedKeys)
+        {
+            if (controllerMappings.ContainsKey(key))
+            {
+                controllerMappings[key].Execute();
+            }
+        }
+    }
+}
+
