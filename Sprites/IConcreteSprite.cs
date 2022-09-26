600bf9654260e2124ce64fa61f255023c2c7cb67
Nicholas Trambitas <47403010+ntrambitas12@users.noreply.github.com>
2022-09-26 18:09:27 -0400

    Update IConcreteSprite.cs

--- Sprites/IConcreteSprite.cs
+++ Sprites/IConcreteSprite.cs
@@ -8,18 +8,9 @@ using System.Threading.Tasks;
 
 public interface IConcreteSprite : ISprite
 {
-    //public int currentFrame { get; set; }
-    //public int totalFrames { get; set; }
-    //public int spritePos { get; set; }
     public ISpriteState still { get; set; }
     public ISpriteState moving { get; set; }
     public ISpriteState damaged { get; set; }
-    //public SpriteBatch spriteBatch { get; set; }
-    //public Vector2 screenCord { get; set; }
-    //public List<Texture2D> textureToDraw { get; set; }
-    //void Draw();
-    //void Update();
-    //void SetSpriteAction(SpriteAction action);
     void SetSpriteState(SpriteAction action, ISpriteState spriteState);
 
 }
