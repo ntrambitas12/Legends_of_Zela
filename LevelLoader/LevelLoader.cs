07c791481993b833f9acf24988a02f1cf5fe2bf9
Nicholas Trambitas <ntrambitas12@gmail.com>
2022-10-18 01:25:37 -0400

    working XML parser

--- LevelLoader/LevelLoader.cs
+++ LevelLoader/LevelLoader.cs
@@ -12,7 +12,7 @@ public class LevelLoader
     {
         settings = new XmlReaderSettings();
         settings.IgnoreWhitespace = true;
-        reader = XmlReader.Create("C:\\Users\\Ben\\source\\repos\\CSE3902Project\\LevelLoader\\RoomTest.xml");
+        reader = XmlReader.Create("C:\\Users\\ntram\\source\\repos\\CSE3902Project\\LevelLoader\\RoomTest.xml");
         parseTypes = new List<(string, string)>()
         {
             ("Blocks", "Block"),
@@ -27,12 +27,16 @@ public class LevelLoader
         int yPos;
         String name;
         int roomObjectType;
+        bool read = false;
 
-        foreach (var parseType in parseTypes)
+        // foreach (var parseType in parseTypes)
+        //{
+        reader.ReadToFollowing("Blocks");
+        read = reader.ReadToDescendant("Block");
+
+        if (read)
         {
-            reader.ReadToFollowing(parseType.Item1);
-
-            while (reader.ReadToFollowing(parseType.Item2))
+            do
             {
                 reader.ReadToDescendant("xPos");
                 xPos = reader.ReadElementContentAsInt();
@@ -42,11 +46,55 @@ public class LevelLoader
                 name = reader.ReadElementContentAsString();
                 reader.ReadToNextSibling("RoomObjectType");
                 roomObjectType = reader.ReadElementContentAsInt();
-                /* This is where you call the corresponding method from spritefactory
-                * and add that ISprite to the roomobject into correct list using add
-                */
+                reader.Read();
             }
+            while (reader.ReadToNextSibling("Block"));
         }
+        /* This is where you call the corresponding method from spritefactory
+        * and add that ISprite to the roomobject into correct list using add
+        */
+
+        reader.ReadToFollowing("Enemies");
+       read = reader.ReadToDescendant("Enemy");
+        if (read)
+        {
+            do
+            {
+                reader.ReadToDescendant("xPos");
+                xPos = reader.ReadElementContentAsInt();
+                reader.ReadToNextSibling("yPos");
+                yPos = reader.ReadElementContentAsInt();
+                reader.ReadToNextSibling("Name");
+                name = reader.ReadElementContentAsString();
+                reader.ReadToNextSibling("RoomObjectType");
+                roomObjectType = reader.ReadElementContentAsInt();
+                reader.Read();
+            }
+            while (reader.ReadToNextSibling("Enemy"));
+        }
+
+
+        reader.ReadToFollowing("Items");
+        read = reader.ReadToDescendant("Item");
+        if (read)
+        {
+            do
+            {
+                reader.ReadToDescendant("xPos");
+                xPos = reader.ReadElementContentAsInt();
+                reader.ReadToNextSibling("yPos");
+                yPos = reader.ReadElementContentAsInt();
+                reader.ReadToNextSibling("Name");
+                name = reader.ReadElementContentAsString();
+                reader.ReadToNextSibling("RoomObjectType");
+                roomObjectType = reader.ReadElementContentAsInt();
+                reader.Read();
+            }
+            while (reader.ReadToNextSibling("Item"));
+        }
+
+
+        //}
     }
 
     /*
