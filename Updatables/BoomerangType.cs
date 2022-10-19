5a92ebbe7942e377e0f404a148497e34ef2ee5f6
Kyle Hoefker <hoefker.2@osu.edu>
2022-10-19 16:31:55 -0400

    Boomerang tracks player on the way back

--- Updatables/BoomerangType.cs
+++ Updatables/BoomerangType.cs
@@ -11,6 +11,8 @@ public class BoomerangType : IItemType
     private int counter;
     private int distance;
     private int speed;
+    private Vector2 shooterCord;
+    private Vector2 directionBack;
 
     public BoomerangType(IProjectile projectile)
     {
@@ -27,49 +29,59 @@ public class BoomerangType : IItemType
         distance = projectile.Distance();
         speed = 5;
 
-        if (counter == distance / 3)
+        //if (counter == distance / 2)
+        //{
+        //    switch (direction)
+        //    {
+        //        case 0: // left to right
+        //            projectile.SetDirection(1);
+        //            break;
+        //        case 1: // right to left
+        //            projectile.SetDirection(0);
+        //            break;
+        //        case 2: // up to down
+        //            projectile.SetDirection(3);
+        //            break;
+        //        case 3: // down to up
+        //            projectile.SetDirection(2);
+        //            break;
+        //        default:
+        //            break;
+        //    }
+        //}
+
+        if (counter >= distance / 3)
         {
+            speed = 3;
+            shooterCord = projectile.Owner().screenCord;
+            directionBack = shooterCord - changeCord;
+            directionBack.Normalize();
+
+            changeCord += 3 * directionBack;
+        }
+        else
+        {
+            //direction = projectile.Direction();
             switch (direction)
             {
-                case 0: // left to right
-                    projectile.SetDirection(1);
+                case 0:
+                    changeCord.X -= speed;
                     break;
-                case 1: // right to left
-                    projectile.SetDirection(0);
+                case 1:
+                    changeCord.X += speed;
                     break;
-                case 2: // up to down
-                    projectile.SetDirection(3);
+                case 2:
+                    changeCord.Y -= speed;
                     break;
-                case 3: // down to up
-                    projectile.SetDirection(2);
+                case 3:
+                    changeCord.Y += speed;
                     break;
                 default:
                     break;
             }
         }
-
-        if (counter >= distance / 3) speed = 3;
-
-        direction = projectile.Direction();
-        switch (direction)
-        {
-            case 0:
-                changeCord.X -= speed;
-                break;
-            case 1:
-                changeCord.X += speed;
-                break;
-            case 2:
-                changeCord.Y -= speed;
-                break;
-            case 3:
-                changeCord.Y += speed;
-                break;
-            default:
-                break;
-        }
         projectile.SetPosition(changeCord);
-
+        
         if (shouldDraw)
         {
             fireProjectile.Execute();
