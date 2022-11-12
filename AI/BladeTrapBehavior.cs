d3f93168eba3190d202e515b091caa38ebda1c11
Kyle Hoefker <hoefker.2@osu.edu>
2022-11-12 05:47:52 -0500

    Blade Trap works correctly

--- AI/BladeTrapBehavior.cs
+++ AI/BladeTrapBehavior.cs
@@ -19,12 +19,14 @@ public class BladeTrapBehavior : IAI
     private bool pauseEnemies;
     private SpriteAction enemyAction;
     private IRoomObject currRoom;
+    private Vector2 corner;
 
     //--------------------------------INITIALIZER--------------------------------
     //must be passed an entity for 'this' to be attached to, and size of collider 'colliderDimensions'
     public BladeTrapBehavior(ISprite entity)
     {
         this.entity = entity;
+        corner = entity.screenCord;
     }
 
     //--------------------------------METHODS--------------------------------
@@ -49,7 +51,7 @@ public class BladeTrapBehavior : IAI
                     cord.Y -= 2;
                 }
                 entity.screenCord = cord;
-            } else if (Math.Abs(link.screenCord.Y - entity.screenCord.Y) < 30 && (entity.screenCord.Y == 370 + currRoom.BaseCord.Y || entity.screenCord.Y == 178 + currRoom.BaseCord.Y))
+            } else if (Math.Abs(link.screenCord.Y - entity.screenCord.Y) < 10 && (entity.screenCord.Y == 370 + currRoom.BaseCord.Y || entity.screenCord.Y == 178 + currRoom.BaseCord.Y))
             {
                 Vector2 cord = entity.screenCord;
                 //move left or right
@@ -74,31 +76,84 @@ public class BladeTrapBehavior : IAI
     private void returnToCorner(ISprite entity)
     {
         Vector2 cord = entity.screenCord;
-        if (entity.screenCord.X == 208 + currRoom.BaseCord.X || entity.screenCord.X == 560 + currRoom.BaseCord.X)
+        if (corner.X == 208 + currRoom.BaseCord.X && corner.Y == 178 + currRoom.BaseCord.Y)
         {
-            //move up or down
-            if (entity.screenCord.Y < currRoom.BaseCord.Y + (370 + 178) / 2)
+            //move to left up
+            if (entity.screenCord.X > 208 + currRoom.BaseCord.X)
             {
-                //move up
-                cord.Y -= 2;
-            } else
-            {
-                //move down
-                cord.Y += 2;
-            }
-        } else
-        {
-            //move left or right
-            if (entity.screenCord.X < currRoom.BaseCord.X + (208 + 560) / 2)
-            {
-                //move left
+                // move left
                 cord.X -= 2;
             } else
             {
-                //move right
+                // move up
+                cord.Y -= 2;
+            }
+        } else if (corner.X == 560 + currRoom.BaseCord.X && corner.Y == 178 + currRoom.BaseCord.Y)
+        {
+            //move to right up
+            if (entity.screenCord.X < 560 + currRoom.BaseCord.X)
+            {
+                // move right
                 cord.X += 2;
             }
+            else
+            {
+                // move up
+                cord.Y -= 2;
+            }
         }
+        else if (corner.X == 208 + currRoom.BaseCord.X && corner.Y == 370 + currRoom.BaseCord.Y)
+        {
+            //move to left down
+            if (entity.screenCord.X > 208 + currRoom.BaseCord.X)
+            {
+                // move left
+                cord.X -= 2;
+            }
+            else
+            {
+                // move down
+                cord.Y += 2;
+            }
+        }
+        else
+        {
+            //move to right down
+            if (entity.screenCord.X < 560 + currRoom.BaseCord.X)
+            {
+                // move right
+                cord.X += 2;
+            } else
+            {
+                // move down
+                cord.Y += 2;
+            }
+        }
+        //if (entity.screenCord.X == 208 + currRoom.BaseCord.X || entity.screenCord.X == 560 + currRoom.BaseCord.X)
+        //{
+        //    //move up or down
+        //    if (entity.screenCord.Y < currRoom.BaseCord.Y + (370 + 178) / 2)
+        //    {
+        //        //move up
+        //        cord.Y -= 2;
+        //    } else
+        //    {
+        //        //move down
+        //        cord.Y += 2;
+        //    }
+        //} else
+        //{
+        //    //move left or right
+        //    if (entity.screenCord.X < currRoom.BaseCord.X + (208 + 560) / 2)
+        //    {
+        //        //move left
+        //        cord.X -= 2;
+        //    } else
+        //    {
+        //        //move right
+        //        cord.X += 2;
+        //    }
+        //}
         entity.screenCord = cord;
     }
 
